ee61ab0694d7be582a6e8854c57935c1f3d62562
강병우 <kbw2204@icloud.com>
2020-08-06 23:32:11 +0900

    뷰 컨트롤러 생성 및 스토리보드 연결

---
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoComposeViewController.swift
+//  RxMemo
+//
+//  Created by 강병우 on 2020/08/06.
+//  Copyright © 2020 강병우. All rights reserved.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MemoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

d12b63e6318f78fb6912a8a343377f7a89b70c49
강병우 <kbw2204@icloud.com>
2020-08-06 23:07:28 +0900

    메모 model 추가

---
+++ RxMemo/Model/Memo.swift
@@ -0,0 +1,27 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by 강병우 on 2020/08/06.
+//  Copyright © 2020 강병우. All rights reserved.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
+

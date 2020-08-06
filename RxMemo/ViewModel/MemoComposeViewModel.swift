5342a798b3bfbff1fc8148b1504b30dccf99a8d6
강병우 <kbw2204@icloud.com>
2020-08-06 23:45:43 +0900

    ViewController 추가

---
+++ RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemoComposeViewModel.swift
+//  RxMemo
+//
+//  Created by 강병우 on 2020/08/06.
+//  Copyright © 2020 강병우. All rights reserved.
+//
+
+import Foundation
+
+class MemoComposeViewModel {
+
+}
